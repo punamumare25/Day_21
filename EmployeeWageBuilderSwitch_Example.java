81a799bdcf806490fda9c8de23dd7f2382ea3832
Punam <punamumare25@gmail.com>
2020-08-14 11:26:24 +0530

    Employee Wage Builder Switch

---
+++ EmployeeWageBuilderSwitch_Example.java
@@ -0,0 +1,26 @@
+public class EmployeeWageBuilderSwitch {
+  public static final int IS_PART_TIME=1;
+  public static final int IS_FULL_TIME =2;
+  public static final int EMP_RATE_PER_HOUR=20;
+
+
+  public static void main(String args[]){
+      int emphrs=0;
+      int empwage=0;
+
+      double empcheck = Math.floor(Math.random() * 10) % 3;
+
+      switch ((int)empcheck){
+      case IS_FULL_TIME:
+      emphrs=8;
+      break;
+      case IS_PART_TIME:
+      emphrs=4;
+      break;
+      default:
+      emphrs=0;
+      }
+      empwage=emphrs*EMP_RATE_PER_HOUR;
+      System.out.println("EMPWAGE="+empwage);
+      }
+   }
